f0c798a5df2f72baefbcae263f7f992b2300fe50
JuanDNJ <juandevnjv@gmail.com>
2025-02-10 18:59:04 +0100

    first commit

---
+++ src/api/firebase.ts
@@ -0,0 +1,41 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GithubAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.API_KEY,
+  authDomain: import.meta.env.AUTH_DOMAIN,
+  projectId: import.meta.env.PROJECT_ID,
+  storageBucket: import.meta.env.STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
+  appId: import.meta.env.APP_ID
+}
+
+export const app = initializeApp(firebaseConfig);
+
+
+
+const provider = new GithubAuthProvider();
+// provider.addScope('repo');
+
+
+const auth = getAuth();
+signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+    const credential = GithubAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+
+    // The signed-in user info.
+    const user = result.user;
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GithubAuthProvider.credentialFromError(error);
+    // ...
+  });
---
+++ src/components/github-login/GItHubLogin.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react'
+import GItHubLogin from './GItHubLogin';
+import { vi } from 'vitest'
+
+describe('Renderizado inicial', () => {
+  it('debe renderizar el botón con el texto correcto', () => {
+    render(<GItHubLogin />);
+    const texto = screen.getByText(/Sign in with GItHub/i);
+
+    expect(texto).toBeInTheDocument();
+  });
+
+  it('debe llamar a la función onClick cuando se hace clic en el botón', () => {
+    const onClickMock = vi.fn()
+    render(<GItHubLogin onClick={onClickMock} />);
+
+    const boton = screen.getByText(/Sign in with GItHub/i);
+    boton.click();
+
+    expect(onClickMock).toHaveBeenCalled();
+  });
+
+});
